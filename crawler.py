f0feccddad7a8c36f947bcfc7219308bedb16094
combi_jihoon <combi_jihoon@naver.com>
2021-04-14 23:24:12 +0900

    Success double click

--- crawler.py
+++ crawler.py
@@ -1,4 +1,7 @@
 import time
+
+from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,13 +14,23 @@ driver.get(url)
 
 actionChains = ActionChains(driver)
 
-
+# kakaomap review 찾기
 driver.find_element_by_css_selector(
     '.query.tf_keyword').send_keys('용인시 '+'마약생고기')
 driver.find_element_by_css_selector('.query.tf_keyword').send_keys(Keys.ENTER)
 time.sleep(2)
 
-# click doesn't work....
-review = driver.find_element_by_css_selector('.review')
-# review = driver.find_element(By.ID, 'numberofreview')
+
+# double click
+review = driver.find_element_by_css_selector(
+    '#info\.search\.place\.list > li.PlaceItem.clickArea.PlaceItem-ACTIVE > div.rating.clickArea > a').action()
 actionChains.double_click(review).perform()
+
+# ratings = []
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+kakao_rating = soup.select(
+    '.grade_star').text
+
+print(kakao_rating)
