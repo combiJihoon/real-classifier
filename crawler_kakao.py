71e93797a61b94a4e83e3e0e517a8b67d05d0a67
combi_jihoon <combi_jihoon@naver.com>
2021-04-18 16:06:31 +0900

    Crawl rating number, txt_comment, date at kakaomap

--- crawler_kakao.py
+++ crawler_kakao.py
@@ -34,4 +34,32 @@ time.sleep(2)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 ratings = soup.select('.grade_star')
 
+# 별점 출력
 print(ratings[1].text)
+
+# 최고 별점 리뷰 및 최저 별점 리뷰(+날짜) 출력
+# data-page가 끝날 때까지 반복한 후 별점이 가장 낮을 때 그 내용 출력(클 때도 마찬가지)
+all_reviews = soup.select(
+    '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li')
+
+# print(all_reviews)
+review_info = []
+for review in all_reviews:
+    temp = []
+    rating = review.select_one(
+        '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div > div > em').text
+    txt_comment = review.select_one(
+        '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > p > span').text
+    date = review.select_one(
+        '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > div > span.time_write').text
+    temp.append(rating)
+    temp.append(txt_comment)
+    temp.append(date)
+    review_info.append(temp)
+
+# mArticle > div.cont_evaluation > div.evaluation_review > ul > li:nth-child(1) > div.comment_info
+print(review_info)
+
+driver.quit()
+# print('최저 별점을 남긴 고객들의 리뷰 내용입니다: ')
+# print('최고 별점을 남긴 고객들의 리뷰 내용입니다: ')
