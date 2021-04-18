01e29dbf9c47670e44e29da57f412a08ad936327
combi_jihoon <combi_jihoon@naver.com>
2021-04-18 16:56:51 +0900

    Crawl review info going to the next page until final(Failed yet)

--- crawler_kakao.py
+++ crawler_kakao.py
@@ -8,6 +8,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome(r"/Users/jihun/Mywork/RealClassifier/chromedriver")
 url = 'https://map.kakao.com/'
@@ -39,27 +43,68 @@ print(ratings[1].text)
 
 # 최고 별점 리뷰 및 최저 별점 리뷰(+날짜) 출력
 # data-page가 끝날 때까지 반복한 후 별점이 가장 낮을 때 그 내용 출력(클 때도 마찬가지)
-all_reviews = soup.select(
-    '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li')
 
-# print(all_reviews)
-review_info = []
-for review in all_reviews:
-    temp = []
-    rating = review.select_one(
-        '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div > div > em').text
-    txt_comment = review.select_one(
-        '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > p > span').text
-    date = review.select_one(
-        '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > div > span.time_write').text
-    temp.append(rating)
-    temp.append(txt_comment)
-    temp.append(date)
-    review_info.append(temp)
+# # 별점, 리뷰, 날짜 출력
+# all_reviews = soup.select(
+#     '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li')
 
-# mArticle > div.cont_evaluation > div.evaluation_review > ul > li:nth-child(1) > div.comment_info
-print(review_info)
+# # print(all_reviews)
+# review_info = []
+# for review in all_reviews:
+#     temp = []
+#     rating = review.select_one(
+#         '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div > div > em').text
+#     txt_comment = review.select_one(
+#         '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > p > span').text
+#     date = review.select_one(
+#         '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > div > span.time_write').text
+#     temp.append(rating)
+#     temp.append(txt_comment)
+#     temp.append(date)
+#     review_info.append(temp)
 
-driver.quit()
+
+# print(review_info)
+
+# page 이동하며 review_info 크롤링
+while True:
+    review_by_page = []
+    for i in range(1, 20):
+        # 별점, 리뷰, 날짜 출력
+        all_reviews = soup.select(
+            '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li')
+
+        # print(all_reviews)
+        review_info = []
+        for review in all_reviews:
+            temp = []
+            rating = review.select_one(
+                '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div > div > em').text
+            txt_comment = review.select_one(
+                '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > p > span').text
+            date = review.select_one(
+                '#mArticle > div.cont_evaluation > div.evaluation_review > ul > li > div.comment_info > div > span.time_write').text
+            temp.append(rating)
+            temp.append(txt_comment)
+            temp.append(date)
+            review_info.append(temp)
+
+        review_by_page.append(review_info)
+
+        time.sleep(2)
+        # 다음 페이지 클릭
+        next_page_btn = driver.find_element_by_xpath(
+            '//*[@id = "mArticle"]/div[6]/div[4]/div/a['+str(i)+']')
+        try:
+            next_page_btn.click()
+        except NoSuchElementException:
+            break
+        time.sleep(2)
+
 # print('최저 별점을 남긴 고객들의 리뷰 내용입니다: ')
 # print('최고 별점을 남긴 고객들의 리뷰 내용입니다: ')
+
+
+driver.quit()
+
+print(review_by_page)
