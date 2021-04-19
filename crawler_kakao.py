fd414826f3ece03fb13737772711b4f4707b6a6f
combi_jihoon <combi_jihoon@naver.com>
2021-04-19 21:54:20 +0900

    Crawl all the review info from kakaomap successfully

--- crawler_kakao.py
+++ crawler_kakao.py
@@ -13,6 +13,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
+start = time.time()
+
 driver = webdriver.Chrome(r"/Users/jihun/Mywork/RealClassifier/chromedriver")
 url = 'https://map.kakao.com/'
 driver.get(url)
@@ -72,8 +74,10 @@ review_by_page = []
 
 # 다음 페이지 클릭
 
-
-for i in range(1, 6):
+# i는 1~ , j는 1~5 단위의 페이지
+i = j = 1
+pageNum = 1
+while True:
     try:
         # 별점, 리뷰, 날짜 출력
         soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -96,12 +100,30 @@ for i in range(1, 6):
 
         review_by_page.append(review_info)
 
-        print('현재 페이지: '+str(i))
-        print(review_info)
+        print('현재 페이지: '+str(pageNum))
+        # print(review_info)
 
+        # # 페이지가 5의 배수인지 확인 : 5의 배수면 '다음'을 누를 것
+        # if i % 5 == 0:
+        #     # 다음 누르기
+        #     btn_next = driver.find_element_by_class_name('.btn_next')
+        #     driver.execute_script("arguments[0].click();", btn_next)
+        #     time.sleep(2)
+        # # 5의 배수가 아니라면 숫자 누를 것
+        # else:
         element = driver.find_element_by_xpath(
             '//*[@id = "mArticle"]/div[5]/div[4]/div/a['+str(i)+']')
         driver.execute_script("arguments[0].click();", element)
+        if i == 5 and j == 1:
+            i = 2
+            j += 1
+        elif i == 6 and j >= 2:
+            i = 2
+            j += 1
+        else:
+            i += 1
+        pageNum += 1
+
         time.sleep(2)
 
         # WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
@@ -116,12 +138,22 @@ for i in range(1, 6):
     except NoSuchElementException:
         break
 
+end = time.time()
+
+# 몇 초 걸렸는지 확인
+total_time = int(end-start)
+
 driver.quit()
-# break
-# time.sleep(2)
 
 
 # print('최저 별점을 남긴 고객들의 리뷰 내용입니다: ')
 # print('최고 별점을 남긴 고객들의 리뷰 내용입니다: ')
 
+print('------------------------------')
+print('전체 리뷰 크롤링 결과')
+print('걸린시간: ' + str(total_time) + '초')
+print('총 페이지 수 : ' + str(pageNum))
+print('------------------------------')
 print(review_by_page)
+
+print(len(review_by_page))
